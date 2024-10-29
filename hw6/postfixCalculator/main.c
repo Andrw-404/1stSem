d98e0a1a7cf9db91e54c62d938969f9fbc296bc7
Andrew Kalinin <Fipett2407@gmail.com>
2024-10-30 01:02:51 +0300

    Написать программу для вычисления арифметического выражения в постфиксной форме. С клавиатуры вводится последовательность цифр (для простоты) и операций +, -, *, /, представляющая выражение в постфиксной форме, должен выводиться результат вычисления. Например, на тесте 9 6 - 1 2 + * должно получиться 9.

---
+++ hw6/postfixCalculator/main.c
@@ -0,0 +1,29 @@
+#include "stack.h"
+#include "postfixCalculator.h"
+#include "testStack.h"
+#include "testsForPostfixCalculator.h"
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    setlocale(LC_ALL, "RUS");
+    if (!testCreateStack() || !testIsEmpty() || !testPop() || !testPush()) {
+        printf("!Стек не прошёл тесты!\n");
+        return 10;
+    }
+
+    if (!calculatorTest() || !testForOperatorChecking()) {
+        printf("!Основные функции не прошли тесты!\n");
+        return 11;
+    }
+
+    char string[100] = " ";
+    printf("Введите выражение: ");
+    fgets(string, 100, stdin);
+    string[strcspn(string, "\n")] = 0;
+    int answer = postfixCalculator(string);
+    printf("Ответ: %d\n", answer);
+}
