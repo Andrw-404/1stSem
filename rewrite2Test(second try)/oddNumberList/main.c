f9a63cbed8d20597e558b7ce59b8e7a2c5d5edeb
Andrew Kalinin <Fipett2407@gmail.com>
2024-12-23 16:25:56 +0300

    add: start

---
+++ rewrite2Test(second try)/oddNumberList/main.c
@@ -0,0 +1,14 @@
+#include "list.h"
+#include "stdio.h"
+
+int main(void) {
+    List* list = createList();
+    addLast(list, 1);
+    addLast(list, 2);
+    addLast(list, 3);
+    addLast(list, 4);
+    addLast(list, 5);
+
+    remov
+
+}
